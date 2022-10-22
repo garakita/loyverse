b958ddb276e697a65316cba4600d59511840fcdf
garakita <50921296+garakita@users.noreply.github.com>
2022-10-22 17:51:35 +0700

    Update request.php

--- request.php
+++ request.php
@@ -14,11 +14,11 @@ $token = refreshToken($refresh_token);
 define('ACCESS_TOKEN', $token);
 
 
-//$start_date_time = date('Y-m-d 00:00:01');
-//$end_date_time = date('Y-m-d 23:59:59');
+$start_date_time = date('Y-m-d 00:00:01');
+$end_date_time = date('Y-m-d 23:59:59');
 
-$start_date_time = date('22-10-21 00:00:01');
-$end_date_time = date('22-10-21 23:59:59');
+//$start_date_time = date('22-10-21 00:00:01');
+//$end_date_time = date('22-10-21 23:59:59');
 
 $start_date_time = date('Y-m-d\TH:i:s.sZ', strtotime($start_date_time));
 $end_date_time = date('Y-m-d\TH:i:s.sZ', strtotime($end_date_time));
